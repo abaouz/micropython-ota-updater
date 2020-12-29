3932b4c093061bebfcd984f9ab1c5bf89cabde54
Abaouz Hamouche <ahamouche@vegafrance.eu>
2020-12-29 15:56:50 +0100

    update

--- deploy_fld.sh
+++ deploy_fld.sh
@@ -7,7 +7,12 @@ echo "\n Load firmware"
 esptool.py --chip esp32 --port /dev/ttyUSB0 write_flash -z 0x1000 esp32-idf3-20200902-v1.13.bin
 sleep 3
 echo "\n load embedded software"
-python pyboard.py --device /dev/ttyUSB0 -f cp boot.py secret.py webrepl_cfg.py main.py app :
+python pyboard.py --device /dev/ttyUSB0 -f mkdir app
+python pyboard.py --device /dev/ttyUSB0 -f cp boot.py secret.py webrepl_cfg.py main.py :
+python pyboard.py --device /dev/ttyUSB0 -f cp app/__init__.py :app/__init__.py
+python pyboard.py --device /dev/ttyUSB0 -f cp app/httoclient.py:app/httoclient.py
+python pyboard.py --device /dev/ttyUSB0 -f cp app/ota_updater.py:app/ota_updater.py
+python pyboard.py --device /dev/ttyUSB0 -f cp app/start.py:app/start.py
 sleep 3
 echo "\n Display Sensor ID (mac adresse)"
 python pyboard.py --device /dev/ttyUSB0 -c 'import machine'
